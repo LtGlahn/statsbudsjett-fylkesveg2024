0da3d7b3c37332a62e9a670f0d39d7377a8b8794
LtGlahn <jensen72@gmail.com>
2023-08-24 08:31:42 +0200

    Mangler filtrering på Eier-egenskap

--- kode/script_rekkverk.py
+++ kode/script_rekkverk.py
@@ -11,6 +11,15 @@ sok.filter( {'vegsystemreferanse' : 'Fv' } )
 rekk = nvdbgeotricks.nvdbsok2GDF( sok ) 
 rekk.to_file( '../resultater/vegnettFV.gpkg', layer='rekkverk', driver='GPKG')
 
+# OBS! Ved datauttak juni 2023 filtrerte vi ikke for 
+# Eier = Fylkeskommune, Stat, Statens vegvesen og (tom)
+# Ref vianova-rapport s 32. Dette må rettes opp. 
+# Blir noe slikt som 
+# rekk['Eier'].fillna( '(tom)', inplace=True )
+# rekk = rekk[ (rekk[rekk['Eier'] == 'Fylkeskommune'] ) | 
+#              (rekk[rekk['Eier'] == 'Stat, Statens vegvesen'] ) | 
+#              (rekk[rekk['Eier'] == '(tom)'] )]
+
 med_lengde = rekk[ ~rekk['Lengde'].isnull()].copy()
 uten_lengde = rekk[ rekk['Lengde'].isnull()].copy()
 med_lengde.drop_duplicates( subset='nvdbId', inplace=True )
